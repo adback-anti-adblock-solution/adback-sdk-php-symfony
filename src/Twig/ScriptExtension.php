43802e9c5265438a86b4e9e652a8f31abdb4ce5e
Jérémy Gignon <jeremy@gignon.fr>
2018-05-03 17:15:11 +0200

    Using GlobalGenerator to generate all scripts at once and keep existing functions in place

--- src/Twig/ScriptExtension.php
+++ src/Twig/ScriptExtension.php
@@ -2,38 +2,21 @@
 
 namespace Adback\ApiClientBundle\Twig;
 
-use Adback\ApiClient\Generator\ScriptGeneratorInterface;
+use Adback\ApiClientBundle\Generator\GlobalGenerator;
 
 /**
  * Class ScriptExtension
  */
 class ScriptExtension extends \Twig_Extension
 {
-    protected $productGenerator;
-    protected $messageGenerator;
-    protected $analyticsGenerator;
-    protected $iabBannerGenerator;
-    protected $autopromoBannerGenerator;
+    protected $globalGenerator;
 
     /**
-     * @param ScriptGeneratorInterface $analyticsScriptGenerator
-     * @param ScriptGeneratorInterface $messageScriptGenerator
-     * @param ScriptGeneratorInterface $autopromoBannerScriptGenerator
-     * @param ScriptGeneratorInterface $iabBannerScriptGenerator
-     * @param ScriptGeneratorInterface $productScriptGenerator
+     * @param GlobalGenerator $globalGenerator
      */
-    public function __construct(
-        ScriptGeneratorInterface $analyticsScriptGenerator,
-        ScriptGeneratorInterface $messageScriptGenerator,
-        ScriptGeneratorInterface $autopromoBannerScriptGenerator,
-        ScriptGeneratorInterface $iabBannerScriptGenerator,
-        ScriptGeneratorInterface $productScriptGenerator
-    ) {
-        $this->productGenerator = $productScriptGenerator;
-        $this->messageGenerator = $messageScriptGenerator;
-        $this->analyticsGenerator = $analyticsScriptGenerator;
-        $this->autopromoBannerGenerator = $autopromoBannerScriptGenerator;
-        $this->iabBannerGenerator = $iabBannerScriptGenerator;
+    public function __construct(GlobalGenerator $globalGenerator)
+    {
+        $this->globalGenerator = $globalGenerator;
     }
 
     /**
@@ -41,10 +24,7 @@ class ScriptExtension extends \Twig_Extension
      */
     public function generateScripts()
     {
-        return sprintf('<script>%s</script><script>%s</script>',
-            $this->analyticsGenerator->generate(),
-            $this->messageGenerator->generate()
-        );
+        return $this->globalGenerator->generate();
     }
 
     /**
@@ -52,7 +32,7 @@ class ScriptExtension extends \Twig_Extension
      */
     public function generateAutopromoBannerScript()
     {
-        return sprintf('<script>%s</script>', $this->autopromoBannerGenerator->generate());
+        return '';
     }
 
     /**
@@ -60,7 +40,7 @@ class ScriptExtension extends \Twig_Extension
      */
     public function generateIabBannerScript()
     {
-        return sprintf('<script>%s</script>', $this->iabBannerGenerator->generate());
+        return '';
     }
 
     /**
@@ -68,7 +48,7 @@ class ScriptExtension extends \Twig_Extension
      */
     public function generateProductScript()
     {
-        return sprintf('<script>%s</script>', $this->productGenerator->generate());
+        return '';
     }
 
     /**
